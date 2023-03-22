900a23ac44244da691e4e081f34b012b873d4003
Cris Acosta <cristina.acosta@gamanza.com>
2023-03-22 08:48:40 -0600

    no message

--- src/index.tsx
+++ src/index.tsx
@@ -3,31 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Route } from "react-router-dom";
 import LastName from "./LastName";
 
 // Find all widget divs
 const WidgetDivs = document.querySelectorAll(".json_widget");
-
-const getBrowserRouter = (Div: Element) => {
-  return createBrowserRouter([
-    {
-      path: "/",
-      element: <App domElement={Div} />,
-    },
-    {
-      path: "/lastName",
-      element: <LastName domElement={Div} />,
-    },
-  ]);
-};
 
 // Inject our React App into each
 WidgetDivs.forEach((Div) => {
   const root = createRoot(Div);
   root.render(
     <React.StrictMode>
-      <RouterProvider router={getBrowserRouter(Div)} />
+      <Route path="/" element={<App domElement={Div} />} />
+      <Route path="/lastName" element={<LastName domElement={Div} />} />
     </React.StrictMode>
   );
 });
