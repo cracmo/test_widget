19889b4eed6fa4ab54046b3bdd44e733409504f1
Cris Acosta <cristina.acosta@gamanza.com>
2023-03-22 10:11:14 -0600

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css';
+import "./App.css";
 
 function App({ domElement }: { domElement: Element }) {
-  const json = domElement.getAttribute('data-json') || '';
+  const json = domElement.getAttribute("data-json") || "";
   const info = JSON.parse(json);
   return (
     <div className="App">
       <p>{info.firstName}</p>
       <p>{info.lastName}</p>
-      <button onClick={() => alert(info.phone[0])}>click me</button>
+      <p>{info.username}</p>
+      <button onClick={() => alert(info.phoneNumber)}>click me</button>
     </div>
   );
 }
