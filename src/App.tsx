c310f4ef632cf1f2046d16c645434d6eadf876d7
Cris Acosta <cristina.acosta@gamanza.com>
2023-03-22 09:53:43 -0600

    Revert "added react-router-dom"
    This reverts commit 94f4d9fd2230e4cd1fee73819d36e7b01394a49d.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import "./App.css";
+import './App.css';
 
 function App({ domElement }: { domElement: Element }) {
-  const json = domElement.getAttribute("data-json") || "";
+  const json = domElement.getAttribute('data-json') || '';
   const info = JSON.parse(json);
   return (
     <div className="App">
       <p>{info.firstName}</p>
+      <p>{info.lastName}</p>
       <button onClick={() => alert(info.phone[0])}>click me</button>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,19 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import LastName from "./LastName";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 // Find all widget divs
-const WidgetDivs = document.querySelectorAll(".json_widget");
-
-const getBrowserRouter = (Div: Element) => {
-  return createBrowserRouter([
-    {
-      path: "/",
-      element: <App domElement={Div} />,
-    },
-    {
-      path: "/lastName",
-      element: <LastName domElement={Div} />,
-    },
-  ]);
-};
+const WidgetDivs = document.querySelectorAll('.json_widget');
 
 // Inject our React App into each
 WidgetDivs.forEach((Div) => {
-  const root = createRoot(Div);
-  root.render(
+  ReactDOM.render(
     <React.StrictMode>
-      <RouterProvider router={getBrowserRouter(Div)} />
-    </React.StrictMode>
+      <App domElement={Div} />
+    </React.StrictMode>,
+    Div
   );
 });
 
